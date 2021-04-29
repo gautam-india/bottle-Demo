a2b5cc13b080035d06ec24a4f436ec1b048d86a6
GAUTAM KUMAR SINGH <81463037+gautam-india@users.noreply.github.com>
2021-04-29 22:55:22 +0530

    update

---
+++ Assets/Scripts/Load Menu/SetScreenSize.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetScreenSize : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject.DontDestroyOnLoad(this);
+        Screen.SetResolution(720, 1280, true, 60);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            if (Input.GetKey(KeyCode.Home) || Input.GetKey(KeyCode.Escape) || Input.GetKey(KeyCode.Menu))
+            {
+                Application.Quit();
+                return;
+            }
+        }
+    }
+}
